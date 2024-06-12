b8ee2566356dd70e7cb54d6df74026c7f83dfe8f
Courtney Conde <159712256+cococnde@users.noreply.github.com>
2024-06-11 23:04:54 -0500

    fixed multiple errors that caused the svg to be undefined

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let inquirer;
 const fs = require('fs');
-const jest = require('jest');
+// Importing the shapes
 
 const { Square, Circle, Triangle } = require('./lib/shapes.js');
 // SVG Function
@@ -8,13 +8,13 @@ function createSVG(answers) {
     let shape;
     switch(answers.shape) {
         case 'Square':
-            shape = new Square(answers.logoColor);
+            shape = new Square(answers.text, answers.textColor, answers.logoColor);
             break;
         case 'Circle':
-            shape = new Circle(answers.logoColor);
+            shape = new Circle(answers.text, answers.textColor, answers.logoColor);
             break;
         case 'Triangle':
-            shape = new Triangle(answers.logoColor);
+            shape = new Triangle(answers.text, answers.textColor, answers.logoColor);
             break;
     }
     return shape.createSVG();
@@ -28,7 +28,7 @@ import('inquirer').then(module => {
         {
             type: 'input',
             message: 'Please enter up to three alphabet characters for your logo',
-            name: 'letters'
+            name: 'text'
         },
         {
             type: 'input',
@@ -37,7 +37,7 @@ import('inquirer').then(module => {
         },
         {
         type: 'list',
-        message: 'Please select s logo shape ',
+        message: 'Please select a logo shape ',
         name: 'shape',
         choices: ['Square', 'Circle', 'Triangle']
     },
@@ -49,8 +49,16 @@ import('inquirer').then(module => {
     }
     
 ]).then(answers => {
-    const svg = createSVG(answers);
-    fs.writeFile('logo.svg', svg, err => {
+    const svg = createSVG({
+        
+        text: answers.text, 
+        textColor: answers.textColor, 
+        logoColor: answers.logoColor, 
+        shape: answers.shape
+    
+    });
+
+    fs.writeFile('./examples/logo.svg', svg, err => {
         if(err) {
             console.error(err);
     } else {
@@ -58,10 +66,23 @@ import('inquirer').then(module => {
     }
     });
 
+    if(answers.shape === 'Circle') {
+
+    let myCircle = new Circle(answers.text, answers.textColor ,answers.logoColor);
+    } else if(answers.shape === 'Square') {
+        let mySquare = new Square(answers.text, answers.textColor, answers.logoColor);
+    } else if(answers.shape === 'Triangle') {
+        let myTriangle = new Triangle(answers.text, answers.textColor, answers.logoColor);
+    }
 }).catch(err => {
     console.error(err);
 });
+
+
 })
+
+let jest;
+
 import('jest').then(module => {
     jest = module.default || module;
 }).catch(err => {
